6bc5f75cc2ee79191f38b468c0e5a29bb3d14fe0
Easley <jessicaeasley@jessicas-air.attlocal.net>
2023-02-23 23:31:43 -0600

    defined password characters parameters

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,31 @@
 // Assignment code here
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-
+ 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword("#generatePassword");
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
 
+// Password Characters
+const numberCharacters = "0123456789" .split("");
+const specialCharacters = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~".split("");
+const upperCase = "abcdefghijklmnopqrstuvwxyz".split("");
+const lowerCase = lowerCaseChars.map(char => char.toUpperCase());
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var passwordLength = 8-128;
+var password = "";
+
+for (var i = 0; i <= passwordLength; i++) {
+  var randomNumber = Math.floor(Math.random() * chars.length);
+  password += chars.substring(randomNumber, randomNumber +1);
+ }
