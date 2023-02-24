b470cf4e9dcb8513df30632dc2b565162ba64c7f
Easley <jessicaeasley@jessicas-air.attlocal.net>
2023-02-23 23:59:31 -0600

    added password options for user input

--- Develop/script.js
+++ Develop/script.js
@@ -1,26 +1,45 @@
 // Assignment code here
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
- 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword("#generatePassword");
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
+var passwordText = document.querySelector("#password");
 
 // Password Characters
 const numberCharacters = "0123456789" .split("");
 const specialCharacters = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~".split("");
 const upperCase = "abcdefghijklmnopqrstuvwxyz".split("");
-const lowerCase = lowerCaseChars.map(char => char.toUpperCase());
+const lowerCase = lowerCase.map(char => char.toUpperCase());
 
 
 // var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var passwordLength = 8-25;
+var passwordLength = 8-128;
 var password = "";
+
+// Password Options User Input
+
+function PasswordOptions() {
+const useNum = confirm("Use special characters?");
+const useSpecial = confirm("Use numbers?");
+const useLower = confirm("Use lowercase characters?");
+const useUpper = confirm("Use uppercase characters?");
+const charCount = parseInt(prompt("How many characters do you want?"), 12);
+
+return {
+  useNum,
+  useSpecial,
+  useLower,
+  useUpper,
+  charCount
+
+}
+}
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword("#generatePassword");
+  // var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
 
 for (var i = 0; i <= passwordLength; i++) {
   var randomNumber = Math.floor(Math.random() * chars.length);
